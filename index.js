c4564c0816980e76ff0bcfc802f6a95ea47f9c98
awoldt <abreckwoldt23@gmail.com>
2021-12-26 11:19:16 -0500

    artists page is fully funcitonal

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const dbConnect = require("./DB_connect");
+const SongModel = require('./SongModel');
+
 
 app.set("view engine", "ejs");
 
@@ -16,10 +18,36 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/", "index.html"));
 });
 
-app.get("/artists", (req, res) => {
+app.get("/artists", async (req, res) => {
   res.status(200);
+
+
+
+  const songs = await SongModel.find();
+
+  const a = new Array(); //artist_queries
+  const b = new Array(); //artists name
+  await songs.forEach(x => {
+    if(a.indexOf(x.artist_query) === -1) {
+      a.push(x.artist_query)
+    }
+  })
+  await songs.forEach(x => {
+    if(b.indexOf(x.artist) === -1) {
+      b.push(x.artist)
+    }
+  })
+
+
+  console.log(a.sort());
+  console.log(b.sort());
+
+
+
+
   res.render("artists", {
-    data: "heyyy",
+    artist_names: b.sort(),
+    artists_links: a.sort(),
   });
 });
 
