001e3231afad0ac0befe42c224f970456758e9dc
abreckwoldt23@gmail.com <alex@alexs-mbp.lan>
2021-12-26 16:24:37 -0500

    each artist has own page now

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const dbConnect = require("./DB_connect");
-const SongModel = require('./SongModel');
-
+const SongModel = require("./SongModel");
 
 app.set("view engine", "ejs");
 
@@ -21,29 +20,23 @@ app.get("/", (req, res) => {
 app.get("/artists", async (req, res) => {
   res.status(200);
 
-
-
   const songs = await SongModel.find();
 
   const a = new Array(); //artist_queries
   const b = new Array(); //artists name
-  await songs.forEach(x => {
-    if(a.indexOf(x.artist_query) === -1) {
-      a.push(x.artist_query)
+  await songs.forEach((x) => {
+    if (a.indexOf(x.artist_query) === -1) {
+      a.push(x.artist_query);
     }
-  })
-  await songs.forEach(x => {
-    if(b.indexOf(x.artist) === -1) {
-      b.push(x.artist)
+  });
+  await songs.forEach((x) => {
+    if (b.indexOf(x.artist) === -1) {
+      b.push(x.artist);
     }
-  })
-
+  });
 
   console.log(a.sort());
   console.log(b.sort());
-
-
-
 
   res.render("artists", {
     artist_names: b.sort(),
@@ -51,7 +44,19 @@ app.get("/artists", async (req, res) => {
   });
 });
 
-app.use(apiRoutes);
+app.get("/artists/:artist_name", async (req, res) => {
+  res.status(200);
+
+  const artistData = await SongModel.find({
+    artist_query: req.params.artist_name,
+  });
+
+  res.render("artistPage", {
+    artist_data: artistData,
+  });
+});
+
+app.use(apiRoutes); //all api backend
 
 app.listen(8080, () => {
   console.log("App running on port 8080");
