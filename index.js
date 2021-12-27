e44f7de912a73be83d0300fe656f6535999d6039
abreckwoldt23@gmail.com <alex@alexs-mbp.lan>
2021-12-27 11:58:23 -0500

    fixed bug on artist page where artist links and names where not lining up

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require("path");
 const dbConnect = require("./DB_connect");
 const SongModel = require("./SongModel");
-const Profile = require('./ArtistProfile')
+const Profile = require("./ArtistProfile");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -25,22 +25,26 @@ app.get("/artists", async (req, res) => {
 
   const songs = await SongModel.find();
 
-  const a = new Array(); //artist_queries
-  const b = new Array(); //artists name
-  await songs.forEach((x) => {
-    if (a.indexOf(x.artist_query) === -1) {
-      a.push(x.artist_query);
+  //get all artist queries first
+  const artistLinks = new Array();
+  await songs.forEach(async (x) => {
+    if (artistLinks.indexOf(x.artist_query) === -1) {
+      await artistLinks.push(x.artist_query);
     }
   });
-  await songs.forEach((x) => {
-    if (b.indexOf(x.artist) === -1) {
-      b.push(x.artist);
-    }
-  });
+  console.log(artistLinks.sort());
+
+  //findone of each artist name using artist query above
+  const artistNames = new Array();
+  for (i = 0; i < artistLinks.length; ++i) {
+    const name = await SongModel.findOne({ artist_query: artistLinks[i] });
+    console.log(name.artist);
+    await artistNames.push(name.artist);
+  }
 
   res.render("artists", {
-    artist_names: b.sort(),
-    artists_links: a.sort(),
+    artist_names: artistNames,
+    artists_links: artistLinks,
   });
 });
 
@@ -63,14 +67,24 @@ app.get("/artists/:artist_name", async (req, res) => {
     }
   }
 
-  const profile = await Profile.find({artist_query: req.params.artist_name});
-  console.log('artist profile')
-  console.log(profile)
+  const profile = await Profile.find({ artist_query: req.params.artist_name });
+  console.log("artist profile");
+  console.log(profile);
+
+  //if artist has only explicit songs to show, make sure search engines cannot index
+  var shouldIndex = false; //will be true if seach engine SHOULD index
+  for (var i = 0; i < artistData.length; ++i) {
+    if (artistData[i].explicit !== true) {
+      shouldIndex = true;
+      break;
+    }
+  }
 
   res.render("artistPage", {
     artist_data: artistData,
     artist_profile: profile,
     has_explicit_songs: hasExplicitSongs,
+    should_index: shouldIndex,
   });
 });
 
