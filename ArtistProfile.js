cdc14a02866b2bda178915c8dfbdb32d681b996d
awoldt <abreckwoldt23@gmail.com>
2021-12-30 12:14:37 -0500

    artist profile images give attribution and are modified for commerical use

--- ArtistProfile.js
+++ ArtistProfile.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const profileSchema = new mongoose.Schema({
   artist_query: String,
-  profile_img: String
+  profile_img: String,
+  img_attribution: Object
 });
 
 const ArtistProfile =
