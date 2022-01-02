abcad95792cfce10ec941ba7c244fb39fb061ea3
awoldt <abreckwoldt23@gmail.com>
2022-01-02 09:09:04 -0500

    ready for production

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+var favicon = require("serve-favicon");
 
 const express = require("express");
 const app = express();
@@ -7,6 +8,7 @@ const dbConnect = require("./DB_connect");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 dbConnect();
 
@@ -22,6 +24,16 @@ app.get("/", (req, res) => {
 app.use(artistRoutes);
 app.use(yearRoutes);
 app.use(apiRoutes); //all api backend
+
+app.get("/sitemap.xml", (req, res) => {
+  res.status(200);
+  res.sendFile(path.join(__dirname + "/", "sitemap.xml"));
+});
+
+app.get("/robots.txt", (req, res) => {
+  res.status(200);
+  res.sendFile(path.join(__dirname + "/", "robots.txt"));
+});
 
 app.listen(8080, () => {
   console.log("App running on port 8080");
