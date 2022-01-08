34e7684c346091b2d74c992c5ec808d3b3375789
awoldt <abreckwoldt23@gmail.com>
2022-01-07 20:07:45 -0500

    stable

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -64,10 +64,15 @@ router.get("/api/filter", async (req, res) => {
       //EXPLICIT
     } else if (query.hasOwnProperty("explicit")) {
       if (query.explicit !== "true" && query.explicit !== "false") {
-        res.status(400);
-        res.json({
-          message: "explicit query can only be true or false",
-        });
+        if (query.explicit == "both") {
+          res.status(200);
+          res.json({ message: "both" });
+        } else {
+          res.status(400);
+          res.json({
+            message: "explicit query can only be true or false",
+          });
+        }
       } else {
         const results = await SongModel.find({
           explicit: query.explicit,
