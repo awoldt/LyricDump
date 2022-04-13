c870afcedda8a00d08c2b9b4884ae7c16eb98c1c
unknown <abreckwoldt23@gmail.com>
2022-04-13 11:01:02 -0800

    added adsense

--- DB_connect.js
+++ DB_connect.js
@@ -4,6 +4,7 @@ async function databaseConnect() {
   //if connection does not already exist
   if (mongoose.connection.readyState !== 1) {
     try {
+      console.log(process.env.MONGOOSE_KEY)
       await mongoose.connect(process.env.MONGOOSE_KEY);
       console.log("successfully connected to database!");
     } catch (e) {
